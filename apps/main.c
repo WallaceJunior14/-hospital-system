69d437287eadeb99361b01c062a2f32f6c7afada
joao-tinelli <joao_tinelli@outlook.com>
2024-11-05 13:46:23 -0300

    [MENU] updated

--- apps/main.c
+++ apps/main.c
@@ -38,12 +38,13 @@ int main() {
     while (1) {
         puts("---- HOSPITAL-SYSTEM ----\n");
         puts("1. Register a new patient\n");
-        puts("2. Delete a patient\n");
-        puts("3. Search for a patient by Id\n");
-        puts("4. Search for a patient by Name\n");
-        puts("5. List all patients by Id\n");
-        puts("6. List all patients by Name\n");
-        puts("7. Exit and generate report\n");
+        puts("2. Delete a patient by Id\n");
+        puts("3. Delete a patient by Name\n");
+        puts("4. Search for a patient by Id\n");
+        puts("5. Search for a patient by Name\n");
+        puts("6. List all patients by Id\n");
+        puts("7. List all patients by Name\n");
+        puts("8. Exit and generate report\n");
 
         scanf("%d", &choice);
 
@@ -52,26 +53,44 @@ int main() {
             puts("  Enter id, name, age and medical condition: ");
             scanf("%d %s %d %s", &line_id, line_name, &line_age, line_medical_condition);
 
-            root_id = insertNodeById(root_id, line_id, line_name, line_age, line_medical_condition);
-            root_name = insertNodeByName(root_name, line_id, line_name, line_age, line_medical_condition);
+            if (searchNodeById(root_id, line_id) == NULL && searchNodeByName(root_name, line_name) == NULL){
+                root_id = insertNodeById(root_id, line_id, line_name, line_age, line_medical_condition);
+                root_name = insertNodeByName(root_name, line_id, line_name, line_age, line_medical_condition);
+                puts("  Patient registered successfully!\n");
 
-            puts("  Patient registered successfully!\n");
+            } else {
+                puts("  Patient already exists.\n");
+            }
+
             break;
 
         case 2:
-            puts("  Enter id and name: ");
-            scanf("%d %s", &line_id, line_name);
+            puts("  Enter id: ");
+            scanf("%d", &line_id);
 
-            if (searchNodeById(root_id, line_id) == NULL || searchNodeByName(root_name, line_name) == NULL) {
+            if (searchNodeById(root_id, line_id) == NULL) {
                 puts("  Patient not found.\n");
+
             } else {
                 root_id = deleteNodeById(root_id, line_id);
-                root_name = deleteNodeByName(root_name, line_name);
                 puts("  Patient deleted successfully!\n");
             }
             break;
 
         case 3:
+            puts("  Enter name: ");
+            scanf("%s", line_name);
+
+            if (searchNodeByName(root_name, line_name) == NULL) {
+                puts("  Patient not found.\n");
+
+            } else {
+                root_name = deleteNodeByName(root_name, line_name);
+                puts("  Patient deleted successfully!\n");
+            }
+            break;
+
+        case 4:
             puts("  Enter id: ");
             scanf("%d", &line_id);
 
@@ -82,7 +101,7 @@ int main() {
             }
             break;
 
-        case 4:
+        case 5:
             puts("  Enter name: ");
             scanf("%s", line_name);
 
@@ -93,20 +112,20 @@ int main() {
             }
             break;
         
-        case 5:
+        case 6:
             puts("Traversing BST by Id: ");
             traverseId(root_id);
             puts("\n");
             break;
 
-        case 6:
+        case 7:
             puts("Traversing BST by Name: ");
             traverseName(root_name);
             puts("\n");
             break;
 
-        case 7:
-            puts("Deallocating BST and generating report of patients.\n");
+        case 8:
+            puts("Deallocating BST and generating report of patients...\n");
             // TODO: Add code to generate report here
             deallocateById(&root_id);
             deallocateByName(&root_name);
