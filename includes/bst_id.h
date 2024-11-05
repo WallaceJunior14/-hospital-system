52987b277d97959c9d01d25c305fd677dc4ff52c
WallaceJunior14 <wallacepfj1409@gmail.com>
2024-11-05 15:45:53 -0300

    [Includes] Criado um brief de todas as funcoes

--- includes/bst_id.h
+++ includes/bst_id.h
@@ -2,22 +2,58 @@
 #define H_BST_ID
 
 /**
- * @brief 
+ * @brief node;
+ * @typedef PatientNodeId
  */
 typedef struct _patient PatientNodeId;
 
+/**
+ * @brief Create a node in the treee;
+ * @param id, *name, age, *medical_condition;
+ * @return PatientNodeId;
+*/
 PatientNodeId *createNodeById(const unsigned int id, const char *name, const unsigned int age, const char *medical_condition);
 
+/**
+ * @brief Insert a node in the treee;
+ * @param *root, id, *name, age, *medical_condition;
+ * @return PatientNodeId;
+*/
 PatientNodeId *insertNodeById(PatientNodeId *root, const unsigned int id, const char *name, const unsigned int age, const char *medical_condition);
 
+/**
+ * @brief Search for a node in the tree by its id;
+ * @param *root, id;
+ * @return PatientNodeId;
+*/
 PatientNodeId *searchNodeById(PatientNodeId *root, const unsigned int id);
 
+/**
+ * @brief Search for the smallest value within the tree;
+ * @param *root;
+ * @return PatientNodeId;
+*/
 PatientNodeId *findMinID(PatientNodeId *root);
 
+/**
+ * @brief Delete a node in the tree by its id;
+ * @param *root, id;
+ * @return PatientNodeId;
+*/
 PatientNodeId *deleteNodeById(PatientNodeId *root, const unsigned int id);
 
+/**
+ * @brief Cross the tree in order;
+ * @param *root;
+ * @return void;
+*/
 void traverseId(const PatientNodeId *root); 
 
+/**
+ * @brief Deallocate the tree;
+ * @param *root;
+ * @return void;
+*/
 void deallocateById(PatientNodeId **root);
 
 #endif
--- includes/bst_name.h
+++ includes/bst_name.h
@@ -2,22 +2,58 @@
 #define H_BST_NAME
 
 /**
- * @brief 
+ * @brief node;
+ * @typedef PatientNodeName
  */
 typedef struct _patient PatientNodeName;
 
+/**
+ * @brief Create a node in the treee;
+ * @param id, *name, age, *medical_condition;
+ * @return PatientNodeName;
+*/
 PatientNodeName *createNodeByName(const unsigned int id, const char *name, const unsigned int age, const char *medical_condition);
 
+/**
+ * @brief Insert a node in the treee;
+ * @param *root, id, *name, age, *medical_condition;
+ * @return PatientNodeName;
+*/
 PatientNodeName *insertNodeByName(PatientNodeName *root, const unsigned int id, const char *name, const unsigned int age, const char *medical_condition);
 
+/**
+ * @brief Search for a node in the trees by its name;
+ * @param *root, *name;
+ * @return PatientNodeName;
+*/
 PatientNodeName *searchNodeByName(PatientNodeName *root, const char *name);
 
+/**
+ * @brief Search for the smallest value within the tree;
+ * @param *root;
+ * @return PatientNodeName;
+*/
 PatientNodeName *findMinName(PatientNodeName *root);
 
+/**
+ * @brief Delete a node in the tree by its name;
+ * @param *root, *name;
+ * @return PatientNodeName;
+*/
 PatientNodeName *deleteNodeByName(PatientNodeName *root, const char *name);
 
+/**
+ * @brief Cross the tree in order;
+ * @param *root;
+ * @return void;
+*/
 void traverseName(const PatientNodeName *root); 
 
+/**
+ * @brief Deallocate the tree;
+ * @param *root;
+ * @return void;
+*/
 void deallocateByName(PatientNodeName **root);
 
 #endif
